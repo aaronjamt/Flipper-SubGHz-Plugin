931ae21deee6ffa0cfe2cc9b431c632ef6f9d032
Aaronjamt <13600347+aaronjamt@users.noreply.github.com>
2025-06-14 23:54:34 +0100

    Remove test app from repo

--- plugin_remote/layers/header_footer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdlib.h>
-
-// Type for the config function
-typedef struct {
-    void (*configure)(void *storage, char *header, size_t header_size, char *footer, size_t footer_size);
-} HeaderFooterStorage;
-
--- plugin_remote/layers/slip.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-// Type for the config function
-typedef struct {
-    void (*configure)(void *storage, uint8_t frame_start, uint8_t frame_end, uint8_t frame_escape, uint8_t transposed_frame_start, uint8_t transposed_frame_end, uint8_t transposed_frame_escape);
-} SLIPStorage;
