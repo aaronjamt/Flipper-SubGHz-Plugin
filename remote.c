78a8425339ab37f7bfaddda0b2469bdf0b16cf91
Aaron Tulino (Aaronjamt) <git@aaronjamt.com>
2025-08-09 19:08:06 -0700

    Cleanup Ran formatter and fix error building

--- remote.c
+++ remote.c
@@ -14,8 +14,8 @@
 const SubGhzDevice* device;
 SubGhzTxRxWorker* subghz_txrx;
 Buffer subghz_tx_buffer;
-FuriThread *tx_worker_thread;
-FuriMutex *tx_worker_mutex;
+FuriThread* tx_worker_thread;
+FuriMutex* tx_worker_mutex;
 bool is_suppressing_charge = false;
 
 PluginManager* manager;
@@ -27,28 +27,30 @@ DataLayer* layers_head = NULL;
 DataLayer* layers_tail = NULL;
 
 typedef enum {
-    WorkerEventStop  = 1 << 1,
-    WorkerEventData  = 1 << 2,
+    WorkerEventStop = 1 << 1,
+    WorkerEventData = 1 << 2,
     WorkerEventFlush = 1 << 3,
     WorkerEventAgain = 1 << 4,
 } WorkerEventFlags;
 
-void append_layer(DataLayer *layer) {
+void append_layer(DataLayer* layer) {
     furi_check(layer);
 
-    if (layers_tail == NULL) {
-        if (layers_head == NULL) {
+    if(layers_tail == NULL) {
+        if(layers_head == NULL) {
             // This is (currently) the only layer
             layers_head = layers_tail = layer;
             layer->next_layer = layer->prev_layer = NULL;
             return;
         } else {
             // This should never be possible
-            FURI_LOG_E(TAG, "Layers has null tail but non-null head! Please report at https://github.com/aaronjamt/Flipper-SubGHz-Plugin/issues/new\n");
+            FURI_LOG_E(
+                TAG,
+                "Layers has null tail but non-null head! Please report at https://github.com/aaronjamt/Flipper-SubGHz-Plugin/issues/new\n");
 
             // Walk list to end to recover
             layers_tail = layers_head;
-            while (layers_tail->next_layer != NULL)
+            while(layers_tail->next_layer != NULL)
                 layers_tail = layers_tail->next_layer;
 
             // Fall through to append below
@@ -62,42 +64,43 @@ void append_layer(DataLayer *layer) {
     layers_tail = layer;
 }
 
-bool remote_load_layer(const char *name, void **storage) {
-    if (manager == NULL) {
+bool remote_load_layer(const char* name, void** storage) {
+    if(manager == NULL) {
         FURI_LOG_E(TAG, "Plugin was not initialized");
         return false;
     }
-    if (name == NULL) {
+    if(name == NULL) {
         FURI_LOG_E(TAG, "Layer name cannot be NULL");
         return false;
     }
-    
+
     // Concatinate the base plugins directory with the provided layer name
-    const char *base_path = APP_ASSETS_PATH("plugins/");
-    char *_path = malloc(strlen(base_path) + strlen(name) + 1);
-    strcpy(_path, base_path);
-    strcat(_path, name);
+    FuriString* path = furi_string_alloc();
+    furi_string_printf(path, "%s/%s", APP_ASSETS_PATH("plugins"), name);
 
     uint32_t plugin_count = plugin_manager_get_count(manager);
-    if (plugin_manager_load_single(manager, _path) == PluginManagerErrorNone) {
+    if(plugin_manager_load_single(manager, furi_string_get_cstr(path)) == PluginManagerErrorNone) {
         // Make sure exactly one more plugin has been loaded
-        if (plugin_manager_get_count(manager) == plugin_count + 1) {
+        if(plugin_manager_get_count(manager) == plugin_count + 1) {
             // Fetch and call entry point's init() method to create the DataLayer*
-            DataLayerEntryPoint *ep = (DataLayerEntryPoint*)plugin_manager_get_ep(manager, plugin_count);
-            DataLayer *layer = ep->init();
+            DataLayerEntryPoint* ep =
+                (DataLayerEntryPoint*)plugin_manager_get_ep(manager, plugin_count);
+            DataLayer* layer = ep->init();
 
             // Add the DataLayer* to the list of layers
             append_layer(layer);
 
             // Share the DataLayer's layer-specific storage if a void** was provided
-            if (storage != NULL) {
+            if(storage != NULL) {
                 *storage = layer->storage;
             }
 
+            furi_string_free(path);
             return true;
         }
     }
 
+    furi_string_free(path);
     return false;
 }
 
@@ -111,10 +114,7 @@ void remote_set_rx_cb(RemoteCallback callback, void* context) {
 void rx_event_callback(void* ctx) {
     UNUSED(ctx);
 
-    Buffer payload = {
-        .data = NULL,
-        .size = 0
-    };
+    Buffer payload = {.data = NULL, .size = 0};
 
     // Read data from Sub-GHz until there's no more available
     int len;
@@ -122,20 +122,20 @@ void rx_event_callback(void* ctx) {
         uint8_t buffer[32];
         len = (int)subghz_tx_rx_worker_read(subghz_txrx, buffer, sizeof(buffer));
         buffer_append(&payload, buffer, len);
-    } while (len);
+    } while(len);
 
     // Pass the data through the layers in reverse order
-    DataLayer *layer = layers_tail;
-    while (layer) {
+    DataLayer* layer = layers_tail;
+    while(layer) {
         // Add any data from the payload buffer to the layer's receive buffer
-        if (payload.size) {
+        if(payload.size) {
             buffer_append(&layer->recv_buffer, payload.data, payload.size);
             buffer_free(&payload);
         }
 
         int processed = layer->recv(layer->storage, layer->recv_buffer, &payload);
 
-        if (processed == 0) {
+        if(processed == 0) {
             // If the layer didn't process any data, skip updating the receive buffer
             layer = layer->prev_layer;
             continue;
@@ -144,12 +144,15 @@ void rx_event_callback(void* ctx) {
         // We're going to remove the processed data, so update the buffer's size
         layer->recv_buffer.size -= processed;
 
-        if (layer->recv_buffer.size == 0) {
+        if(layer->recv_buffer.size == 0) {
             // If the buffer is now empty, free it
             buffer_free(&layer->recv_buffer);
         } else {
             // Otherwise, move the unprocessed data to the beginning of the buffer
-            memmove(layer->recv_buffer.data, layer->recv_buffer.data + processed, layer->recv_buffer.size);
+            memmove(
+                layer->recv_buffer.data,
+                layer->recv_buffer.data + processed,
+                layer->recv_buffer.size);
             layer->recv_buffer.data = realloc(layer->recv_buffer.data, layer->recv_buffer.size);
         }
 
@@ -157,25 +160,25 @@ void rx_event_callback(void* ctx) {
         layer = layer->prev_layer;
     }
 
-    if (payload.size == 0) {
+    if(payload.size == 0) {
         FURI_LOG_W(TAG, "No data to send to callback!\n");
         return;
     }
 
-    if (rx_callback == NULL) {
+    if(rx_callback == NULL) {
         FURI_LOG_W(TAG, "No RX callback set, dropping data!\n");
     } else {
         rx_callback(rx_callback_context, payload.data, payload.size);
     }
 }
 
-void remote_write(uint8_t *data, size_t size) {
-    if (!tx_worker_mutex || !tx_worker_thread) {
+void remote_write(uint8_t* data, size_t size) {
+    if(!tx_worker_mutex || !tx_worker_thread) {
         FURI_LOG_E(TAG, "Sub-GHz TX/RX worker is not running!");
         furi_check(false);
     }
 
-    if (furi_mutex_acquire(tx_worker_mutex, FuriWaitForever) != FuriStatusOk) {
+    if(furi_mutex_acquire(tx_worker_mutex, FuriWaitForever) != FuriStatusOk) {
         FURI_LOG_E(TAG, "Failed to write data: could not lock mutex!");
         furi_check(false);
     }
@@ -189,36 +192,36 @@ void remote_write(uint8_t *data, size_t size) {
 static int32_t tx_worker(void* context) {
     UNUSED(context);
 
-    if (tx_worker_mutex != NULL) {
+    if(tx_worker_mutex != NULL) {
         furi_mutex_free(tx_worker_mutex);
     }
     tx_worker_mutex = furi_mutex_alloc(FuriMutexTypeNormal);
 
-    if (!subghz_txrx) {
+    if(!subghz_txrx) {
         FURI_LOG_E(TAG, "Sub-GHz TX/RX worker is not running!");
         return 0;
     }
 
-    while (1) {
-        uint32_t events =
-            furi_thread_flags_wait(WorkerEventStop | WorkerEventData | WorkerEventFlush | WorkerEventAgain, FuriFlagWaitAny, 1000);
-        
-        if (events == FuriFlagErrorTimeout) events = 0;
+    while(1) {
+        uint32_t events = furi_thread_flags_wait(
+            WorkerEventStop | WorkerEventData | WorkerEventFlush | WorkerEventAgain,
+            FuriFlagWaitAny,
+            1000);
+
+        if(events == FuriFlagErrorTimeout) events = 0;
         furi_check((events & FuriFlagError) == 0);
 
-        if (events == WorkerEventStop) break;
+        if(events == WorkerEventStop) break;
 
-        Buffer payload = {
-            .data = NULL,
-            .size = 0
-        };
+        Buffer payload = {.data = NULL, .size = 0};
 
         // Only read in new data if we can acquire the mutex
-        if ((events & WorkerEventData) && (furi_mutex_acquire(tx_worker_mutex, 25) == FuriStatusOk)) {
+        if((events & WorkerEventData) &&
+           (furi_mutex_acquire(tx_worker_mutex, 25) == FuriStatusOk)) {
             furi_thread_flags_clear(WorkerEventData);
             payload.size = subghz_tx_buffer.size;
 
-            if (payload.size) {
+            if(payload.size) {
                 furi_check(subghz_tx_buffer.data);
                 payload.data = malloc(payload.size);
                 memcpy(payload.data, subghz_tx_buffer.data, payload.size);
@@ -227,19 +230,19 @@ static int32_t tx_worker(void* context) {
             buffer_free(&subghz_tx_buffer);
             furi_mutex_release(tx_worker_mutex);
         }
-        
+
         // Pass the data through the layers
-        DataLayer *layer = layers_head;
-        while (layer) {
+        DataLayer* layer = layers_head;
+        while(layer) {
             // Add any data from the payload buffer to the layer's send buffer
-            if (payload.size) {
+            if(payload.size) {
                 buffer_append(&layer->send_buffer, payload.data, payload.size);
                 buffer_free(&payload);
             }
 
             size_t processed = layer->send(layer->storage, layer->send_buffer, &payload);
 
-            if (processed == 0) {
+            if(processed == 0) {
                 // If the layer didn't process any data, skip updating the send buffer
                 layer = layer->next_layer;
                 continue;
@@ -249,19 +252,23 @@ static int32_t tx_worker(void* context) {
             layer->send_buffer.size -= processed;
 
             // If the buffer is now empty, free it
-            if (layer->send_buffer.size == 0) {
+            if(layer->send_buffer.size == 0) {
                 buffer_free(&layer->send_buffer);
             }
             // Otherwise, move the unprocessed data to the beginning of the buffer
             else {
-                memmove(layer->send_buffer.data, layer->send_buffer.data + processed, layer->send_buffer.size);
-                layer->send_buffer.data = realloc(layer->send_buffer.data, layer->send_buffer.size);
+                memmove(
+                    layer->send_buffer.data,
+                    layer->send_buffer.data + processed,
+                    layer->send_buffer.size);
+                layer->send_buffer.data =
+                    realloc(layer->send_buffer.data, layer->send_buffer.size);
             }
 
             layer = layer->next_layer;
         }
 
-        if (payload.size) {
+        if(payload.size) {
             // Transmit final payload
             while(!subghz_tx_rx_worker_write(subghz_txrx, payload.data, payload.size)) {
                 // Wait a few milliseconds on failure before trying to send again
@@ -269,7 +276,7 @@ static int32_t tx_worker(void* context) {
             }
         }
 
-        if (events & WorkerEventFlush) {
+        if(events & WorkerEventFlush) {
             // TODO: Figure out how to flush the SubGHz TX/RX worker's buffer
         }
     }
@@ -281,7 +288,7 @@ static int32_t tx_worker(void* context) {
 }
 
 void remote_stop() {
-    if (tx_worker_thread != NULL) {
+    if(tx_worker_thread != NULL) {
         furi_thread_flags_set(furi_thread_get_id(tx_worker_thread), WorkerEventStop);
         furi_thread_join(tx_worker_thread);
         furi_thread_free(tx_worker_thread);
@@ -349,7 +356,7 @@ bool remote_init() {
 void remote_free() {
     remote_stop();
 
-    while (layers_head) {
+    while(layers_head) {
         free(layers_head->storage);
         layers_head = layers_head->next_layer;
     }
@@ -361,7 +368,7 @@ void remote_free() {
         is_suppressing_charge = false;
     }
 
-    if (manager != NULL) {
+    if(manager != NULL) {
         plugin_manager_free(manager);
     }
 }
